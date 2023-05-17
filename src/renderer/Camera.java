a3c918077b94d6a0100db3ac47ecb4333be81a39
Benjamin Machlev <beni251@gmail.com>
2023-05-17 17:40:06 +0300

    finish IntegrationTests and constructRay

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -21,6 +21,22 @@ public class Camera {
     private double distance;
 
     /**
+     * Constructor for a new Camera.
+     *
+     * @param position the position
+     * @param vTo      the v to - the vector from the camera to the view plane.
+     * @param vUp      the v up.
+     */
+    public Camera(Point position, Vector vTo, Vector vUp) {
+        if (!isZero(vTo.dotProduct(vUp))) throw new IllegalArgumentException("vTo and vUp must be orthogonal");
+
+        this.position = position;
+        this.vTo = vTo.normalize();
+        this.vUp = vUp.normalize();
+        this.vRight = vTo.crossProduct(vUp).normalize();
+    }
+
+    /**
      * Gets position of the camera.
      *
      * @return the position of the camera.
@@ -84,22 +100,6 @@ public class Camera {
     }
 
     /**
-     * Constructor for a new Camera.
-     *
-     * @param position the position
-     * @param vTo      the v to - the vector from the camera to the view plane.
-     * @param vUp      the v up.
-     */
-    public Camera(Point position, Vector vTo, Vector vUp) {
-        if (!isZero(vTo.dotProduct(vUp))) throw new IllegalArgumentException("vTo and vUp must be orthogonal");
-
-        this.position = position;
-        this.vTo = vTo.normalize();
-        this.vUp = vUp.normalize();
-        this.vRight = vTo.crossProduct(vUp).normalize();
-    }
-
-    /**
      * Sets vp size - the size of the view plane.
      *
      * @param width  the width of the view plane.
@@ -124,15 +124,33 @@ public class Camera {
     }
 
     /**
-     * Construct ray through pixel ray - construct a ray through a pixel in the view plane.
+     * Constructs a ray through a pixel in the view plane.
      *
-     * @param nX - the number of pixels in the width of the view plane.
-     * @param nY - the number of pixels in the height of the view plane.
+     * @param nX  - the number of pixels in the width of the view plane.
+     * @param nY  - the number of pixels in the height of the view plane.
      * @param row - the index of the pixel in the width of the view plane.
      * @param col - the index of the pixel in the height of the view plane.
-     * @return the ray
+     * @return the ray through the pixel.
      */
-    public Ray constructRay(int nX, int nY, int row, int col) {
-        return null;
+    public Ray constructRay(int nX, int nY, int col, int row) {
+        // Calculate the center point of the view plane
+        Point pCenter = position.add(vTo.scale(distance));
+
+        // Calculate the size of each pixel
+        double rX = width / nX;
+        double rY = height / nY;
+
+        // Calculate the coordinates of the current pixel in the view plane
+        double xJ = (col - (nX - 1) / 2d) * rX;
+        double yI = -(row - (nY - 1) / 2d) * rY;
+
+        // Calculate the position of the current pixel in 3D space
+        Point pIJ = pCenter;
+        if (!isZero(xJ)) pIJ = pIJ.add(vRight.scale(xJ));
+        if (!isZero(yI)) pIJ = pIJ.add(vUp.scale(yI));
+
+        // Construct and return the ray from the camera position to the pixel position
+        return new Ray(position, pIJ.subtract(position));
     }
+
 }
---
+++ src/unittest/renderer/IntegrationTests.java
@@ -0,0 +1,127 @@
+package renderer;
+
+import geometries.Intersectable;
+import geometries.Plane;
+import geometries.Sphere;
+import geometries.Triangle;
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Integration tests between creating rays from a camera and
+ * Calculation of sections of a beam with geometric bodies.
+ */
+public class IntegrationTests {
+
+    /**
+     * Integration tests of a camera and a sphere.
+     */
+    @Test
+    public void testSphereAndCamera() {
+        List<Intersectable> spheres = List.of(
+                //TC01: the sphere is in front of the view plane (2 intersection points).
+                new Sphere(1, new Point(0, 0, -3)),
+                //TC02: the view plane is inside the sphere, all rays should intersect twice (18 intersection points).
+                new Sphere(2.5, new Point(0, 0, -2.5)),
+                //TC03: the view plane cross the sphere (10 intersection points).
+                new Sphere(2, new Point(0, 0, -2)),
+                //TC04: the camera is inside the sphere,all rays should intersect only once(9 intersection points).
+                new Sphere(4, new Point(0, 0, -1)),
+                //TC05: the sphere is behind the camera , no ray should intersect(0 intersection point).
+                new Sphere(0.5, new Point(0, 0, 1)));
+
+        var intersectionPointsExcepted = List.of(2, 18, 10, 9, 0);
+
+        testIntersectionWithCamera(spheres, intersectionPointsExcepted);
+    }
+
+    /**
+     * Integration tests of a camera and a plane.
+     */
+    @Test
+    public void testPlaneAndCamera() {
+        List<Intersectable> planes = List.of(
+                //TC01: the plane is parallel with the view plane, all rays should intersect(9 intersection points).
+                new Plane(new Point(0, 0, -2), new Vector(0, 0, 1)),
+                //TC02: the plane is in front of the view plane and cross, all rays should intersect(9 intersection points).
+                new Plane(new Point(0, 0, -1.5), new Vector(0, -0.5, 1)),
+                //TC03: the plane is above the view plane's third row (6 intersection points).
+                new Plane(new Point(0, 0, -3), new Vector(0, -1, 1)));
+        var intersectionPointsExcepted = List.of(9, 9, 6);
+
+        testIntersectionWithCamera(planes, intersectionPointsExcepted);
+    }
+
+    /**
+     * Integration tests of a camera and a triangle.
+     */
+    @Test
+    public void testTriangleAndCamera() {
+        List<Intersectable> triangles = List.of(
+                //TC01:only the center ray should intersect(1 intersection point).
+                new Triangle(new Point(0, 1, -2), new Point(1, -1, -2), new Point(-1, -1, -2)),
+                //TC02: only the center ray and the top-middle ray should intersect(2 intersection points).
+                new Triangle(new Point(0, 20, -2), new Point(1, -1, -2), new Point(-1, -1, -2)));
+
+        var intersectionPointsExcepted = List.of(1, 2);
+
+        testIntersectionWithCamera(triangles, intersectionPointsExcepted);
+    }
+
+    /**
+     * Integration tests of a camera and a plane.
+     *
+     * @param shapes the shapes (sphere, plane, triangle)
+     * @param intersectionPointsExpected the expected number of intersection points
+     */
+    private void testIntersectionWithCamera(List<Intersectable> shapes, List<Integer> intersectionPointsExpected) {
+        // Create the camera with the desired parameters
+        Point cameraPosition = new Point(0, 0, 0.5);  // Set the camera position
+        Vector vTo = new Vector(0, 0, -1);  // Set the direction vector towards the view plane
+        Vector vUp = new Vector(0, 1, 0);  // Set the upward direction vector
+
+        Camera camera = new Camera(cameraPosition, vTo, vUp)
+                .setVPSize(3, 3)  // Set the size of the view plane
+                .setVPDistance(1);  // Set the distance from the camera to the view plane
+
+        // Calculate the expected sum of intersection points
+        int expectedSum = intersectionPointsExpected.stream().mapToInt(Integer::intValue).sum();
+
+        // Initialize the actual sum of intersection points
+        int actualSum = 0;
+
+        // Iterate over each shape
+        for (Intersectable shape : shapes) {
+            // Iterate over each pixel in the view plane
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    // Construct a ray for the current pixel
+                    Ray ray = camera.constructRay(3, 3, col, row);
+
+                    // Calculate the number of intersection points for the ray and the current shape
+                    if (shape.findIntersections(ray) == null)
+                        continue;
+
+                    int intersectionPoints = shape.findIntersections(ray).size();
+
+                    // Add the number of intersection points to the actual sum
+                    actualSum += intersectionPoints;
+                }
+            }
+        }
+
+        // Check if the actual sum matches the expected sum
+        assertEquals(expectedSum, actualSum,
+                "testIntersectionWithCamera() " + shapes.get(0).getClass().getSimpleName() +
+                        ", Wrong number of intersection points");
+    }
+
+
+
+}
