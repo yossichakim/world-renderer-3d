8de16eafd76ee21b200aec1c3a9c07c08eb69685
yossi chakim <melon0533147693@gmail.com>
2023-05-22 16:58:15 +0300

    add ImageWriterTest

--- src/unittest/renderer/ImageWriterTest.java
+++ src/unittest/renderer/ImageWriterTest.java
@@ -1,6 +1,26 @@
 package renderer;
 
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+
 public class ImageWriterTest {
 
+    /**
+     * Test method for
+     * {@link renderer.ImageWriter#writeToImage()}.
+     */
+    @Test
+    void writeToImage() {
 
+        ImageWriter imageWriter = new ImageWriter("test", 800, 500);
+
+        for (int i = 0; i < imageWriter.getNx(); i++) {
+            for (int j = 0; j < imageWriter.getNy(); j++) {
+                if (i % 50 == 0 || j % 50 == 0)
+                    imageWriter.writePixel(i, j, Color.BLACK);
+                else
+                    imageWriter.writePixel(i, j, new Color(105, 178, 255));
+            }
+        }
+    }
 }
