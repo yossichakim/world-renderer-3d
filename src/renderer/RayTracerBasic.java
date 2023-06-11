054819b395d8050072620eb45b2f659df340d9ad
yossi chakim <melon0533147693@gmail.com>
2023-06-11 17:07:19 +0300

    Completion of adding shading

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -5,6 +5,8 @@ import lighting.LightSource;
 import primitives.*;
 import scene.Scene;
 
+import java.util.List;
+
 import static primitives.Util.alignZero;
 import static primitives.Util.isZero;
 
@@ -13,6 +15,11 @@ import static primitives.Util.isZero;
  * Abstract class for ray tracing (basic implementation)
  */
 public class RayTracerBasic extends RayTracerBase {
+
+    /**
+     * The threshold for the recursion level
+     */
+    private static final double DELTA = 0.1;
 
     /**
      * Constructor
@@ -58,8 +65,11 @@ public class RayTracerBasic extends RayTracerBase {
             Vector lightVector = lightSource.getL(gp.point);
             double nl = alignZero(normal.dotProduct(lightVector));
             if (alignZero(nl * nv) > 0 ) {
+                if (unshaded(gp, lightVector, normal, lightSource)) {
                 Color lightIntensity = lightSource.getIntensity(gp.point);
-                color = color.add(lightIntensity.scale(calcDiffusive(material, nl)), lightIntensity.scale(calcSpecular(material, normal, lightVector, nl, vector)));
+                color = color.add(lightIntensity.scale(calcDiffusive(material, nl)),
+                        lightIntensity.scale(calcSpecular(material, normal, lightVector, nl, vector)));
+                }
             }
         }
         return color;
@@ -91,5 +101,30 @@ public class RayTracerBasic extends RayTracerBase {
      */
     private Double3 calcDiffusive(Material material, double nl) {
         return material.kD.scale(nl < 0 ? -nl : nl);
+    }
+
+    /**
+     * function checks if point is shaded
+     *
+     * @param l  light vector
+     * @param n  normal of geometry
+     * @param gp geometry point
+     * @param lightSource light source
+     * @return true if point is shaded
+     */
+    private boolean unshaded(GeoPoint gp, Vector l, Vector n, LightSource lightSource) {
+
+        Vector lightDirection = l.scale(-1); // from point to light source
+        Vector delta = n.scale(n.dotProduct(lightDirection) > 0 ? DELTA : -DELTA);
+        Point point = gp.point.add(delta);
+        Ray shadowRay = new Ray(point, lightDirection);
+        var intersections = scene.geometries.findGeoIntersections(shadowRay);
+        if (intersections == null) return true;
+        double lightDistance = lightSource.getDistance(gp.point);
+        for (GeoPoint geoPoint : intersections) {
+            if (alignZero(geoPoint.point.distance(gp.point) - lightDistance) <= 0)
+                return false;
+        }
+        return true;
     }
 }
